5d122b7f40071f1b072f8ad61043e3fe907164da
Mdyeahia <mdyeahia.khan@gmail.com>
2021-10-08 10:40:00 +0600

    Add project files.

---
+++ Khawla.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Khawla.Web.Startup))]
+namespace Khawla.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
